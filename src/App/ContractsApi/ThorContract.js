8e5d2edbe3f8910c3a9c66bf95c45f849f32a1e7
Jahorse <jahorse@gmail.com>
2022-07-02 08:15:41 -0500

    Added Thor claim

--- src/App/ContractsApi/ThorContract.js
+++ src/App/ContractsApi/ThorContract.js
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers';
 
-import { nodesAbi, feesAbi, taxesAbi } from './abi/thor';
+import { nodesAbi, claimAbi, feesAbi, taxesAbi } from './abi/thor';
 import Contract from './Contract';
 import { getPriceCg } from '../Utils/pricing';
 
@@ -22,6 +22,9 @@ const minTax = {
   Thor: 8,
   Odin: 10,
 }
+const feeContractAddress = '0x9aD4dF248019A36b29202aEC91a1b9aAA863341a';
+const taxContractAddress = '0xD5945eFe9003349f7A05aF6a66D718dE6F087729';
+const claimContractAddress = '0x03E0ff6b3BbF656d2E7157e670465410dB168f48';
 
 class Thor extends Contract {
   metadata = {
@@ -36,8 +39,6 @@ class Thor extends Contract {
     swapLink: 'https://traderjoexyz.com/trade?outputCurrency=0x8f47416cae600bccf9530e9f3aeaa06bdd1caa79#/',
   };
 
-  feeContractAddress = '0x9aD4dF248019A36b29202aEC91a1b9aAA863341a';
-  taxContractAddress = '0xD5945eFe9003349f7A05aF6a66D718dE6F087729';
 
   constructor(provider, walletAddresses, contractAddress, contractName) {
     super(provider, walletAddresses, 'Avalanche');
@@ -60,14 +61,18 @@ class Thor extends Contract {
   }
 
   async claimAll() {
-    console.error('ThorContract has no claimAll().');
-    return null;
+    if (!this.signer) {
+      console.error('Tried calling ProjectX.claimAll() without a valid signer.');
+      return null;
+    }
+    const contract = new ethers.Contract(claimContractAddress, claimAbi, this.signer);
+    return contract.cashoutAll(this.contractName.toUpperCase());
   }
 
   async fetchNodes() {
     const contract = new ethers.Contract(this.contractAddress, nodesAbi, this.jsonRpcProvider);
-    const feesContract = new ethers.Contract(this.feeContractAddress, feesAbi, this.jsonRpcProvider);
-    const taxesContract = new ethers.Contract(this.taxContractAddress, taxesAbi, this.jsonRpcProvider);
+    const feesContract = new ethers.Contract(feeContractAddress, feesAbi, this.jsonRpcProvider);
+    const taxesContract = new ethers.Contract(taxContractAddress, taxesAbi, this.jsonRpcProvider);
 
     const nodes = [];
     for (const walletAddress of this.walletAddresses) {
@@ -107,6 +112,7 @@ class Thor extends Contract {
               rewardAfterTaxes,
               dueDate: new Date(parseInt(dueDate.toString()) * 1000),
               fee: parseInt(fee.toString()) / 1e18,
+              nextProcessingTime: Date.now(),
             });
           }
         }
